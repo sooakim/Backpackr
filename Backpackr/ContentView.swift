a0d4e559d5cd5cc385b36318211fe4c0260b05dd
SooaKim <alice2863@naver.com>
2019-12-07 20:32:53 +0900

    Initial Commit

---
+++ Backpackr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Backpackr
+//
+//  Created by Sooa Kim on 07/12/2019.
+//  Copyright © 2019 Sooa Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
